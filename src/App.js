4c07dc16ce23f78a6843c2cb3bdb0110c8806487
himoura32 <shawn.ahearn@outlook.com>
2022-07-12 11:40:50 -0400

    coded customHook/state/errorHandling/cb&cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { useState } from 'react';
 import TripList from './components/TripList';
 
 function App() {
+  const [showTrips, setShowTrips] = useState(true);
   return (
-    <div className="App">
-      <TripList />
+    <div className='App'>
+      <button onClick={() => setShowTrips(false)}>hide trips</button>
+      {showTrips && <TripList />}
     </div>
   );
 }
--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import './TripList.css';
+import { useFetch } from '../hooks/useFetch';
+import React, { useState } from 'react';
 
 export default function TripList() {
+  const [url, setUrl] = useState('http://localhost:3000/trips');
+  const { data, pending, error } = useFetch(url);
+
   return (
-    <div>
-        <h2>Trip List</h2>
+    <div className='trip-list'>
+      <h2>Trip List</h2>
+      {pending && <div>Loading trips...</div>}
+      {error && <div>{error}</div>}
+      <ul>
+        {data && data.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+      <div className='filters'>
+        <button
+          onClick={() => setUrl('http://localhost:3000/trips/?loc=europe')}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+          All Trips
+        </button>
+      </div>
     </div>
-  )
+  );
 }
